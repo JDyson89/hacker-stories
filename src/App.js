5129a08c54f8ecc1529dcc3860ea79f94cdf76a0
Jasmine Dyson <J.dyson89@yahoo.com>
2021-03-26 01:45:28 -0400

    road to react pages 79-94 commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const list = [
+const initialStories = [
   {
     title: 'React',
     url: 'https://reactjs.org/',
@@ -19,6 +19,14 @@ const list = [
   },
 ];
 
+const getAsyncStories = () =>
+  new Promise(resolve =>
+    setTimeout(
+    () => resolve({ data: { stories: initialStories} }),
+    2000
+    )
+  );
+
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
     localStorage.getItem(key) || initialState
@@ -32,37 +40,41 @@ const useSemiPersistentState = (key, initialState) => {
 };
 
 const App = () => {
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramom, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
-
   const [searchTerm, setSearchTerm] = useSemiPersistentState(
     'search',
     'React'
   );
 
+  const [stories, setStories] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsLoading(true);
+
+    getAsyncStories()
+      .then(result => {
+        setStories(result.data.stories);
+        setIsLoading(false);
+    })
+      .catch(() => setIsError(true));
+  }, []);
+
+  const handleRemoveStory = item => {
+    const newStories = stories.filter(
+      story => item.objectID !== story.objectID
+    );
+
+    setStories(newStories);
+  };
+  
   const handleSearch = event => {
     setSearchTerm(event.target.value);
   };
 
   const searchedStories = stories.filter(story =>
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
-    );
+  );
 
   return (
     <div>
@@ -70,7 +82,8 @@ const App = () => {
 
       <InputWithLabel
         id='search' 
-        value={searchTerm} 
+        value={searchTerm}
+        isFocused
         onInputChange={handleSearch}
       >
         <strong>Search:</strong>
@@ -78,7 +91,16 @@ const App = () => {
       
       <hr />
 
-      <List list={searchedStories} />
+      {isError && <p>Something went wrong ...</p>}
+
+      {isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <List
+          list={searchedStories}
+          onRemoveItem={handleRemoveStory} 
+        />
+      )}
     </div>
   );
 };
@@ -89,32 +111,64 @@ const InputWithLabel = ({
   value,
   type = 'text',
   onInputChange,
+  isFocused,
   children,
-}) => (
+}) => {
+  //A
+  const inputRef = React.useRef();
+  
+  //C
+  React.useEffect(() => {
+    if(isFocused && inputRef.current){
+      //D
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+       
+    return (
     <>
       <label htmlFor={id}>{children}</label>
       &nbsp;
-      <input 
+      <input
+      ref={inputRef} 
         id={id} 
         type={type} 
         value={value}
+        autoFocus={isFocused}
         onChange={onInputChange}
       />
     </>
   );
+}
 
-const List = ({ list }) =>
-  list.map(item => <Item key={item.objectID} item={item} />);
+const List = ({ list, onRemoveItem }) =>
+  list.map(item => (
+    <Item 
+    key={item.objectID} 
+    item={item} 
+    onRemoveItem={onRemoveItem}
+    />
+  ));
 
-const Item = ({ item }) => (
-    <div>
+const Item = ({ item, onRemoveItem }) => (
+  <div>
       <span>
         <a href={item.url}>{item.title}</a>
       </span>
       <span>{item.author}</span>
       <span>{item.num_comments}</span>
       <span>{item.points}</span>
+      <span>
+        <button 
+          type='button' 
+          onClick= {() => {
+            onRemoveItem(item)
+          }}
+        >
+          Dismiss
+        </button>
+      </span>
     </div>
-);
+  );
 
 export default App;
